897e29fe693d372278bed270d3179c4749e9f3b8
Brian Fetter <brian.fetter.dev@gmail.com>
2016-07-15 15:39:27 -0400

    Added SwitchesModule

---
+++ Assets/SwitchesModule/Scripts/Switch.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour
+{
+    public MeshRenderer TopIndicator;
+    public MeshRenderer BottomIndicator;
+    public bool Up
+    {
+        get
+        {
+            return GetComponent<Animator>().GetBool("Up");
+        }
+        set
+        {
+            GetComponent<Animator>().SetBool("Up", value);
+        }
+    }
+
+    public void SetGoal(bool up)
+    {
+        if(up)
+        {
+            TopIndicator.material.color = Color.green;
+            BottomIndicator.material.color = Color.black;
+        }
+        else
+        {
+            TopIndicator.material.color = Color.black;
+            BottomIndicator.material.color = Color.green;
+        }
+    }
+}
---
+++ Assets/SwitchesModule/Scripts/SwitchModule.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public class SwitchModule : MonoBehaviour
+{
+    public Switch[] Switches;
+    SwitchConfiguration goalConfiguration;
+    List<SwitchConfiguration> safeConfigurations;
+
+    void Awake()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        for(int i = 0; i < Switches.Length; i++)
+        {
+            int j = i;
+            Switches[i].GetComponent<KMSelectable>().OnInteract += delegate() { OnToggle(j); return false; };
+        }
+
+        SetInitialConfiguration();
+    }
+
+    protected void OnToggle(int index)
+    {
+        Switches[index].Up = !Switches[index].Up;
+
+        if(GetCurrentConfiguration().Equals(goalConfiguration))
+        {
+            GetComponent<KMBombModule>().HandlePass();
+        }
+        else if(!safeConfigurations.Contains(GetCurrentConfiguration()))
+        {
+            GetComponent<KMBombModule>().HandleStrike();
+            Switches[index].Up = !Switches[index].Up;
+        }
+    }
+
+    protected SwitchConfiguration GetCurrentConfiguration()
+    {
+        bool[] states = new bool[Switches.Length];
+
+        for(int i=0; i < Switches.Length; i++)
+        {
+            states[i] = Switches[i].Up;
+        }
+
+        return new SwitchConfiguration(states);
+    }
+
+    protected void SetInitialConfiguration()
+    {
+        SwitchConfiguration.RuleRandom = new System.Random(1);
+        safeConfigurations = SwitchConfiguration.GetSafeConfigurations(30);
+        goalConfiguration = SwitchConfiguration.GetGoalConfiguration(safeConfigurations);
+        SwitchConfiguration initialConfiguration = SwitchConfiguration.GetInitialConfiguration(safeConfigurations, goalConfiguration);
+        SetGoalIndicators();
+        SetSwitches(initialConfiguration);
+        SwitchConfiguration.GetStrikeConfigurations(safeConfigurations);
+    }
+
+    protected void SetSwitches(SwitchConfiguration config)
+    {
+        for(int i=0; i < config.SwitchStates.Length; i++)
+        {
+            Switches[i].Up = config.SwitchStates[i];
+        }
+    }
+
+    protected void SetGoalIndicators()
+    {
+        for (int i = 0; i < goalConfiguration.SwitchStates.Length; i++)
+        {
+            Switches[i].SetGoal(goalConfiguration.SwitchStates[i]);
+        }
+    }
+}
